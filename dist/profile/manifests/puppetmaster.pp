487fa04b5fd37cc849636a6e6f6d52e79acd9c33
Adam Crews <adam@puppetlabs.com>
2014-04-29 17:06:52 -0700

    Add puppetmaster profile

---
+++ dist/profile/manifests/puppetmaster.pp
@@ -0,0 +1,27 @@
+class profile::puppetmaster {
+  # Mange hiera.yaml
+  file { '/etc/puppetlabs/puppet/hiera.yaml':
+    ensure => file,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => "puppet:///modules/${module_name}/hiera.yaml",
+    notify => Service['pe-httpd'],
+  }
+
+  class { 'r10k':
+    remote           => 'git@github.com:jenkins-infra/jenkins-infra.git',
+    version          => '1.2.1',
+    modulepath       => '/etc/puppetlabs/puppet/environments/$environment/dist:/etc/puppetlabs/puppet/environments/$environment/modules:/opt/puppet/share/puppet/modules',
+    mange_modulepath => true,
+    mcollective      => true,
+  }
+
+  ini_setting { 'Update manifest in puppet.conf':
+    ensure  => present,
+    path    => '/etc/puppetlabs/puppet/puppet.conf',
+    section => 'main',
+    setting => 'manifest',
+    value   => '/etc/puppetlabs/puppet/environments/$environment/manifest/site.pp',
+  }
+}
