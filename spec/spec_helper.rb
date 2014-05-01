b54e869b36800d337e9abaf8e2e105dc3a36453a
R. Tyler Croy <tyler@monkeypox.org>
2014-04-30 21:23:13 -0700

    Add spec/fixtures/modules/*/lib to the rspec $LOAD_PATH to handle custom providers in the catalog
    This helps address autoload issues in rspec-puppet that would otherwise be handled by pluginsync
    
        1) role::puppetmaster
            Failure/Error: should contain_class 'profile::puppetmaster' }
            Puppet::Error:
            Could not autoload puppet/type/yaml_setting: Could not autoload puppet/provider/yaml_setting/mapped: cannot load such file -- puppetx/filemapper on node kiwi
            # ./spec/classes/role/puppetmaster_spec.rb:5:in `block (2 levels) in <top (required)>'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,13 @@ require 'rubygems'
 require 'pry'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
+FIXTURES_PATH = File.expand_path(File.dirname(__FILE__) + '/fixtures')
+# Set up our $LOAD_PATH to properly include custom provider code from modules
+# in spec/fixtures
+$LOAD_PATH.unshift(*Dir["#{FIXTURES_PATH}/modules/*/lib"])
+
 RSpec.configure do |c|
-  c.hiera_config = File.expand_path(File.dirname(__FILE__) + '/fixtures/hiera.yaml')
+  c.hiera_config = File.join(FIXTURES_PATH, 'hiera.yaml')
   c.default_facts = {
     :osfamily => 'Debian',
   }
