acdd7ff7629d32c3a4169b79daf9f0d490e51c3e
R. Tyler Croy <tyler@monkeypox.org>
2014-05-01 14:31:14 -0700

    Clean out old fixtures "just in case"

--- ci/00_setupgems.sh
+++ ci/00_setupgems.sh
@@ -3,3 +3,6 @@
 gem install bundler --no-ri --no-rdoc
 
 bundle install --without development plugins
+
+# clean out old fixtures just in case they were left there by a previous build
+bundle exec rake spec_clean || true
