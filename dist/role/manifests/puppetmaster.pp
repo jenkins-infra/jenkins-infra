98c3d4ff31063155a4e3ddd657c250c09dd7c3c9
R. Tyler Croy <tyler@monkeypox.org>
2014-04-30 17:12:28 -0700

    Properly include profile::accounts in the puppetmaster role
    This commit also includes some simple tests which are marked as pending. I
    missed this because we don't have tests that compile the catalogs with
    profile::puppetmaster. Once we figure out how to make that catalog compilation
    work, we'll be good to unpending these suckers

--- dist/role/manifests/puppetmaster.pp
+++ dist/role/manifests/puppetmaster.pp
@@ -1,7 +1,7 @@
 #
 # role::puppetmaster defines what a node role that should look like
 class role::puppetmaster {
-  include profile::account
+  include profile::accounts
   include profile::puppetmaster
   include profile::r10k
 }
---
+++ spec/classes/role/puppetmaster_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe 'role::puppetmaster' do
+  it { pending 'Need to fix <https://gist.github.com/rtyler/05c3bbe1c33de3745ecd>'; should contain_class 'profile::puppetmaster' }
+  it { pending 'Need to fix <https://gist.github.com/rtyler/05c3bbe1c33de3745ecd>'; should contain_class 'profile::accounts' }
+end
