b39d472a1cae2752b92653ca63c5929614ca6a63
R. Tyler Croy <tyler@monkeypox.org>
2014-05-11 16:48:10 -0700

    Make profile::base remove any remaining code from infra-puppet
    This will prevent the two versions of config management from stomping all over
    one another.

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -4,4 +4,24 @@ class profile::base {
   include profile::accounts
   include profile::ntp
   include profile::sudo
+
+  # Cleaning up after infra-puppet
+  cron { 'pull puppet updates':
+    ensure => absent,
+  }
+
+  cron { 'clean up old puppet logs':
+    ensure => absent,
+  }
+
+  cron { 'clean the repo-update cache':
+    ensure => absent,
+  }
+
+  # Clean up the infra-puppet checkout from the disk
+  file { '/root/infra-puppet':
+    ensure  => absent,
+    recurse => true,
+    force   => true,
+  }
 }
--- spec/classes/profile/base_spec.rb
+++ spec/classes/profile/base_spec.rb
@@ -4,4 +4,11 @@ describe 'profile::base' do
   it { should contain_class 'profile::accounts' }
   it { should contain_class 'profile::ntp' }
   it { should contain_class 'profile::sudo' }
+
+  context 'in order to clean up after infra-puppet' do
+    it { should contain_cron('pull puppet updates').with_ensure('absent') }
+    it { should contain_cron('clean up old puppet logs').with_ensure('absent') }
+    it { should contain_file('/root/infra-puppet').with_ensure('absent') }
+    it { should contain_cron('clean the repo-update cache').with_ensure('absent') }
+  end
 end
