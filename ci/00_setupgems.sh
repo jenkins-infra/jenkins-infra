85dbc44f6bc80d306520708383aea58a3db21ec0
R. Tyler Croy <tyler@monkeypox.org>
2014-05-25 21:50:10 -0700

    Use verbose logging for CI

--- ci/00_setupgems.sh
+++ ci/00_setupgems.sh
@@ -2,7 +2,7 @@
 
 gem install bundler --no-ri --no-rdoc
 
-bundle install --without development plugins
+bundle install --verbose --without development plugins
 
 # clean out old fixtures just in case they were left there by a previous build
 bundle exec rake spec_clean || true
