202f663e276db248eea38ab44723ca67ebf5b0e9
R. Tyler Croy <tyler@monkeypox.org>
2014-05-25 21:48:38 -0700

    Revert "Attempt to install all gems in CI"
    This reverts commit 365560626d09a1171a76adbaa47993d24910d7d7.

--- ci/00_setupgems.sh
+++ ci/00_setupgems.sh
@@ -2,7 +2,7 @@
 
 gem install bundler --no-ri --no-rdoc
 
-bundle install
+bundle install --without development plugins
 
 # clean out old fixtures just in case they were left there by a previous build
 bundle exec rake spec_clean || true
