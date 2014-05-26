e291dc9a441130847a2a03359b15d2fdcf994911
R. Tyler Croy <tyler@monkeypox.org>
2014-05-25 22:02:30 -0700

    Attempt to use a local path for bundler in CI

--- ci/00_setupgems.sh
+++ ci/00_setupgems.sh
@@ -2,7 +2,9 @@
 
 gem install bundler --no-ri --no-rdoc
 
-bundle install --verbose --without development plugins
+mkdir -p vendor/gems
+
+bundle install --verbose --without development plugins --path=vendor/gems
 
 # clean out old fixtures just in case they were left there by a previous build
 bundle exec rake spec_clean || true
