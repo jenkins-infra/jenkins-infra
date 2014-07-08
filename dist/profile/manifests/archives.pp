571d29b41edd040478ef031b95f824891d0eb4d8
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-07-07 18:55:09 -0700

    adding rate limit setting

--- dist/profile/manifests/archives.pp
+++ dist/profile/manifests/archives.pp
@@ -69,6 +69,6 @@ class profile::archives {
     log_level       => 'warn',
     custom_fragment => file("puppet:///modules/${module_name}/archives/vhost.conf"),
     notify          => Service['apache2'],
-    require         => [File['/var/log/apache2/archives.jenkins-ci.org'],Mount['/srv/releases'],Apache::mod['bw']],
+    require         => [File['/var/log/apache2/archives.jenkins-ci.org'],Mount['/srv/releases'],Apache_mod['bw']],
   }
 }
