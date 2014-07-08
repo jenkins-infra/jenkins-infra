163e7bafa6b5c1c449d30faa0b972af13f54b93e
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-07-07 18:25:07 -0700

    defining dependencies.
    Looks like all the dependencies need to be specified manually. That's incredibly stupid.

--- dist/profile/manifests/archives.pp
+++ dist/profile/manifests/archives.pp
@@ -2,25 +2,34 @@
 # Defines an archive server for serving all the archived historical releases
 #
 class profile::archives{
+
+  package { 'lvm2':
+    ensure => present,
+  }
+
   # create volume for storage
   physical_volume { '/dev/xvdb':
-    ensure => present,
+    ensure  => present,
+    require => Package['lvm2'],
   }
 
   volume_group { 'archives':
     ensure           => present,
     physical_volumes => '/dev/xvdb',
+    require      => Physical_volume['/dev/xvdb']
   }
 
   logical_volume { 'releases':
     ensure       => present,
     volume_group => 'archives',
     size         => '100G',
+    require      => Volume_group['archives']
   }
 
   filesystem { '/dev/archives/releases':
     ensure  => present,
     fs_type => 'ext3',
+    require => Logical_volume['releases'],
   }
 
   file { '/srv':
