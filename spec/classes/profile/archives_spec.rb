542f13c4f7c72a2b19d718a5e92faff0b65d2c2d
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-07-11 14:44:56 -0700

    Updating the test
    Looks like the apache module uses the name 'httpd' for everything but
    set the name attribute differently.
    
    Feels bit too much like testing the internals. Perhaps I should just
    check contain_class("apache")

--- spec/classes/profile/archives_spec.rb
+++ spec/classes/profile/archives_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
+require 'pry'
 
 describe 'profile::archives' do
+  let(:facts) {
+    {:operatingsystem => 'Ubuntu', :osfamily => 'Debian' }
+  }
+
   it {
     should contain_filesystem('/dev/archives/releases')
-    should contain_package('apache2')
+    should contain_package('httpd').with( :name => 'apache2' )
   }
 end
