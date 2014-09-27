a43e0352b62ac5d707f3c7107342c77c43680421
R. Tyler Croy <tyler@monkeypox.org>
2014-09-27 13:03:13 -0700

    Rely on notifying the Service['docker-ircbot'] resource for ircbot restarts
    [INFRA-145]

--- spec/classes/profile/jenkinsadmin_spec.rb
+++ spec/classes/profile/jenkinsadmin_spec.rb
@@ -6,6 +6,8 @@ describe 'profile::jenkinsadmin' do
   it { should contain_docker__image 'jenkinsciinfra/ircbot' }
   it { should contain_docker__run 'ircbot' }
 
+  it { should contain_service('docker-ircbot') }
+
   context 'the configuration files' do
     it { should contain_file '/home/ircbot/.github' }
     it { should contain_file '/home/ircbot/.jenkins-ci.org' }
