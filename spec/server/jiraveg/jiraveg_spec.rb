afb26381549e9da529c6a57f9a6ecdc57b095729
Kanstantsin Shautsou <Kanstantsin_Shautsou@epam.com>
2015-03-01 18:55:43 +0300

    Try to fix tests

--- spec/server/jiraveg/jiraveg_spec.rb
+++ spec/server/jiraveg/jiraveg_spec.rb
@@ -1,5 +1,14 @@
-# require_relative './../spec_helper'
-#
-# describe 'jira1' do
-#   it_behaves_like "a jira1 machine"
-# end
+require_relative './../spec_helper'
+
+describe 'jiraveg' do
+  it_behaves_like "a standard Linux machine"
+
+  describe service('jira') do
+    it { should be_enabled   }
+    it { should be_running   }
+  end
+
+  describe port(8080) do
+    it { should be_listening }
+  end
+end
