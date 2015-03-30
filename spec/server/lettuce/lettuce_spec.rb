0309c344da36506cfcbbbc734dc196314a24e22f
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-03-29 21:58:28 -0700

    Initial (mock) confluence deployment
    This change deploys confluence + confluence-cache containers on lettuce, except that instead of real confluence, this version deploys a mock version.

--- spec/server/lettuce/lettuce_spec.rb
+++ spec/server/lettuce/lettuce_spec.rb
@@ -2,4 +2,22 @@ require_relative './../spec_helper'
 
 describe 'lettuce' do
   it_behaves_like "an OSU hosted machine"
+
+  context 'Confluence' do
+    describe port(8009) do
+      it { should be_listening }
+    end
+
+    # test out reverse proxy to JIRA
+    # use '--insecure' flag to skip SSL certificate check, as test boxes won't have the real private key nor the certificate
+    describe command("curl --insecure -L http://wiki.jenkins-ci.org/") do
+      its(:stdout) { should match /Jenkins Wiki/ }
+    end
+    describe command("curl --insecure -L https://wiki.jenkins-ci.org/") do
+      its(:stdout) { should match /Jenkins Wiki/ }
+    end
+    describe command("ls -la /var/log/apache2/wiki.jenkins-ci.org") do
+      its(:stdout) { should match 'access.log.[0-9]{14}' }
+    end
+  end
 end
