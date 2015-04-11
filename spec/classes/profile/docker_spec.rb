b2bf30f8179917a31a8e1d93fca5a27ada4abbaa
R. Tyler Croy <tyler@monkeypox.org>
2015-04-11 11:21:29 -0700

    Allow inter container traffic on the docker0 interface

--- spec/classes/profile/docker_spec.rb
+++ spec/classes/profile/docker_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe 'profile::docker' do
   it { should contain_class 'docker' }
+  it { should contain_firewall('010 allow inter-docker traffic').with_action('accept').with_iniface('docker0') }
 end
