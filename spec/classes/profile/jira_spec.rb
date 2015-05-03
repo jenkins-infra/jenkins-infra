a24d6749430fa63ffcba5c34182650e449ea384b
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-05-03 08:36:17 -0700

    Fixed a test failure

--- spec/classes/profile/jira_spec.rb
+++ spec/classes/profile/jira_spec.rb
@@ -5,5 +5,5 @@ describe 'profile::jira' do
   it { should contain_file '/srv/jira/home' }
   it { should contain_service('docker-jira') }
   it { should contain_file '/var/www/maintenance/maintenance.html' }
-  it { should contain_file '/etc/apache2/sites-available/issues.jenkins-ci.org.maintenance' }
+  it { should contain_file '/etc/apache2/sites-available/issues.jenkins-ci.org.maintenance.conf' }
 end
