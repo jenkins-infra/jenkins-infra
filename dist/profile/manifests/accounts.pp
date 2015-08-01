cd3d1f0ce43f46bf97b6c45efcb761120ab407c9
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-08-01 06:22:38 -0700

    Fixing puppet failures
    Accounts are created on every machine, so atlassian-admins group must
    also exist everywhere, too.

--- dist/profile/manifests/accounts.pp
+++ dist/profile/manifests/accounts.pp
@@ -2,6 +2,10 @@
 # Profile defining all the `account` resources with all our important account
 # information
 class profile::accounts {
+  group { 'atlassian-admins':
+    ensure => present,
+  }
+
   $accounts = hiera_hash('accounts')
   create_resources('account', $accounts)
 }
--- dist/profile/manifests/atlassian.pp
+++ dist/profile/manifests/atlassian.pp
@@ -9,21 +9,15 @@ class profile::atlassian {
   include profile::docker
   include sudo
 
-  $group_name = 'atlassian-admins'
-
   apache::mod { 'proxy':
   }
 
   apache::mod { 'proxy_http':
   }
 
-  group { $group_name:
-    ensure => present,
-  }
-
   sudo::conf { $group_name:
     priority => 10,
-    content  => "%${group_name} ALL=(ALL) NOPASSWD: /usr/sbin/service",
+    content  => "%atlassian-admins ALL=(ALL) NOPASSWD: /usr/sbin/service",
     require  => Group[$group_name],
   }
 }
