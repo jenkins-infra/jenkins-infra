bb840b22adf9dce14a2c8bfe0c504becd1835368
R. Tyler Croy <tyler@monkeypox.org>
2015-12-08 16:33:02 -0800

    Include apache, etc on eggplant for future use

--- spec/classes/role/eggplant_spec.rb
+++ spec/classes/role/eggplant_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe 'role::eggplant' do
   it_should_behave_like 'a standard role'
 
+  it { should contain_class 'profile::apache-misc' }
   it { should contain_class 'profile::sudo::osu' }
 end
