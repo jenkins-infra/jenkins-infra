03f0dae94c291077b60a6c6dbfd315400481d337
R. Tyler Croy <tyler@monkeypox.org>
2015-12-18 13:25:23 -0800

    Refactor the infra-puppet purging code into a separate module so it is not unilaterally applied by base

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -15,26 +15,4 @@ class profile::base {
       'PubkeyAuthentication'   => 'yes',
     },
   }
-
-  # Cleaning up after infra-puppet
-  ##############################################################################
-  cron { 'pull puppet updates':
-    ensure => absent,
-  }
-
-  cron { 'clean up old puppet logs':
-    ensure => absent,
-  }
-
-  cron { 'clean the repo-update cache':
-    ensure => absent,
-  }
-
-  # Clean up the infra-puppet checkout from the disk
-  file { '/root/infra-puppet':
-    ensure  => absent,
-    recurse => true,
-    force   => true,
-  }
-  ##############################################################################
 }
--- spec/classes/profile/base_spec.rb
+++ spec/classes/profile/base_spec.rb
@@ -11,11 +11,4 @@ describe 'profile::base' do
   context 'basic ssh configuration' do
     it { should contain_class 'ssh::server' }
   end
-
-  context 'in order to clean up after infra-puppet' do
-    it { should contain_cron('pull puppet updates').with_ensure('absent') }
-    it { should contain_cron('clean up old puppet logs').with_ensure('absent') }
-    it { should contain_file('/root/infra-puppet').with_ensure('absent') }
-    it { should contain_cron('clean the repo-update cache').with_ensure('absent') }
-  end
 end
