ace2229a3f7a91ef780dbdbca6bb277c572125a5
R. Tyler Croy <tyler@monkeypox.org>
2015-12-22 14:24:32 -0800

    Introduce the basic zone file for jenkins.io into our infrastructure
    Fixes INFRA-485

--- spec/classes/profile/bind_spec.rb
+++ spec/classes/profile/bind_spec.rb
@@ -2,15 +2,26 @@ require 'spec_helper'
 
 describe 'profile::bind' do
   it { should contain_class 'firewall' }
-  it { should contain_class 'profile::docker' }
-  it { should contain_service 'docker-bind' }
 
-  it { should contain_docker__image 'jenkinsciinfra/bind' }
+  context 'docker bind setup' do
+    it { should contain_class 'profile::docker' }
+    it { should contain_service 'docker-bind' }
+    it { should contain_docker__image 'jenkinsciinfra/bind' }
+  end
 
-  it { should contain_file('/etc/bind/local').with({
-    :ensure => 'directory',
-    :purge  => true,
-  }) }
-  it { should contain_file('/etc/bind/local/jenkins-ci.org.zone') }
-  it { should contain_file('/etc/bind/local/named.conf.local') }
+  context 'bind configuration' do
+    it 'should fully manage /etc/bind/local' do
+      expect(subject).to contain_file('/etc/bind/local').with({
+      :ensure => 'directory',
+      :purge  => true,
+      })
+    end
+
+    it { should contain_file('/etc/bind/local/named.conf.local') }
+
+    context 'zones' do
+      it { should contain_file('/etc/bind/local/jenkins-ci.org.zone') }
+      it { should contain_file('/etc/bind/local/jenkins.io.zone') }
+    end
+  end
 end
