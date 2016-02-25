8f63cbe91a3dd588ecabb531669997badb95bff6
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-02-25 14:31:10 -0800

    Wired up to Apache to reverse proxy

--- dist/profile/manifests/jenkins2demo.pp
+++ dist/profile/manifests/jenkins2demo.pp
@@ -2,15 +2,17 @@
 # Run jenkins 2.0 preview in a Docker container to demo its functionality
 class profile::jenkins2demo {
   include profile::docker
+  include profile::apache-misc
 
   $image = 'jenkinsci/jenkins:2.0-alpha-1'
   $user  = 'jenkins2'
+  $site  = 'jenkins2demo'
   $uid   = 2002
 
   docker::image { $image:
   }
 
-  docker::run { 'jenkins2demo':
+  docker::run { $site:
     username        => $uid,
     volumes         => ['/srv/jenkins2demo:/var/jenkins_home'],
     image           => $image,
@@ -29,5 +31,28 @@ class profile::jenkins2demo {
     home_dir => '/srv/jenkins2demo',
     uid      => $uid,
     comment  => 'Runs Jenkins 2 demo',
+  }
+
+  file { "/var/log/apache2/${site}.jenkins-ci.org":
+    ensure => directory,
+  }
+
+  apache::vhost { "${site}.jenkins-ci.org":
+    servername      => "${site}.jenkins-ci.org",
+    port            => '80',
+    docroot         => '/srv/jenkins2demo/userContent',  # bous
+    access_log      => false,
+    error_log_file  => "${site}.jenkins-ci.org/error.log",
+    log_level       => 'warn',
+    custom_fragment => template("${module_name}/jenkins2demo/vhost.conf"),
+
+    notify          => Service['apache2'],
+    require         => [File["/var/log/apache2/${site}.jenkins-ci.org"],
+                        Docker::Run[$site]
+    ],
+  }
+
+  host { "${site}.jenkins-ci.org":
+    ip => '127.0.0.1',
   }
 }
