01e0c9d0f5bd8fa30cd2bc8db4ea8a620fb4058b
R. Tyler Croy <tyler@monkeypox.org>
2016-03-23 13:26:44 -0700

    Introduce profile::accountapp with parameters for configuring the account-app container
    Fixes INFRA-11, INFRA-613

--- dist/profile/manifests/accountapp.pp
+++ dist/profile/manifests/accountapp.pp
@@ -1,5 +1,45 @@
 #
 # Profile defining the necessary resources to provision our LDAP-based
 # accountapp
-class profile::accountapp {
+class profile::accountapp(
+  # all injected from hiera
+  $image_tag,
+  $ldap_url = 'ldap://localhost:389/',
+  $ldap_password = '',
+  $smtp_server = 'localhost',
+  $recaptcha_key = '',
+  $app_url = 'https://accounts.jenkins.io/',
+) {
+  include ::firewall
+  include ::profile::docker
+
+  validate_string($image_tag)
+  validate_string($ldap_url)
+  validate_string($ldap_password)
+  validate_string($smtp_server)
+  validate_string($recaptcha_key)
+
+  file { '/etc/accountapp' :
+    ensure => directory,
+    # Don't allow anything not declared in Puppet to be dropped in there
+    purge  => true,
+  }
+
+  file { '/etc/accountapp/config.properties':
+    ensure  => file,
+    content => template("${module_name}/accountapp/config.properties.erb"),
+    require => File['/etc/accountapp'],
+  }
+
+  docker::image { 'jenkinsciinfra/account-app':
+    image_tag => $image_tag,
+  }
+
+  docker::run { 'account-app':
+    command          => undef,
+    image            => "jenkinsciinfra/account-app:${image_tag}",
+    volumes          => ['/etc/accountapp:/etc/accountapp'],
+    require          => File['/etc/accountapp/config.properties'],
+    extra_parameters => ['--net=host'],
+  }
 }
--- spec/classes/profile/accountapp_spec.rb
+++ spec/classes/profile/accountapp_spec.rb
@@ -1,4 +1,38 @@
 require 'spec_helper'
 
 describe 'profile::accountapp' do
+  it { should contain_class 'firewall' }
+  it { should contain_class 'profile::docker' }
+
+  describe 'accountapp configuration' do
+    it do
+      should contain_file('/etc/accountapp').with({
+        :ensure => :directory,
+      })
+    end
+
+    it { should contain_file('/etc/accountapp/config.properties').with_ensure(:file) }
+  end
+
+  describe 'accountapp docker image' do
+    let(:tag) { 'buildRspec' }
+    let(:params) do
+      {
+        :image_tag => tag,
+      }
+    end
+
+    it 'should have the right image' do
+      expect(subject).to contain_docker__image('jenkinsciinfra/account-app').with({
+        :image_tag => tag,
+      })
+    end
+
+    it 'should run the docker image' do
+      expect(subject).to contain_docker__run('account-app').with({
+        :command => nil,
+        :image => "jenkinsciinfra/account-app:#{tag}",
+      })
+    end
+  end
 end
