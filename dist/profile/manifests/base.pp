d37cf7a3f6e2d67c0cf2929c43d3ecef3a86193e
R. Tyler Croy <tyler@monkeypox.org>
2016-04-07 21:00:42 -0700

    Make sure the base profile provisions on OS X properly
    This is next to impossible to test outside of running on an /actual/ Mac
    attached to an /actual/ Puppet master right now. So this is a first stab at
    things to run in a user specified environment
    
    References INFRA-601

--- dist/profile/manifests/base.pp
+++ dist/profile/manifests/base.pp
@@ -1,24 +1,29 @@
 #
 # Basic profile included in each node
 class profile::base {
+
   include profile::accounts
-  include profile::apt
-  include profile::firewall
-  include profile::ntp
-  include profile::sudo
-  include profile::diagnostics
 
-  class { 'ssh::server':
-    storeconfigs_enabled => false,
-    options              => {
-      'PasswordAuthentication' => 'no',
-      'PubkeyAuthentication'   => 'yes',
-    },
-  }
+  if $::kernel == 'Linux' {
+    include profile::apt
+    # None of these modules support anything other than Linux (apparently)
+    include profile::firewall
+    include profile::ntp
+    include profile::sudo
+    include profile::diagnostics
 
-  class { 'ssh::client':
-    options => {
-      'UseRoaming' => 'no',
-    },
+    class { 'ssh::server':
+      storeconfigs_enabled => false,
+      options              => {
+        'PasswordAuthentication' => 'no',
+        'PubkeyAuthentication'   => 'yes',
+      },
+    }
+
+    class { 'ssh::client':
+      options => {
+        'UseRoaming' => 'no',
+      },
+    }
   }
 }
