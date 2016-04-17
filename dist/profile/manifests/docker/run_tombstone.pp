f6b1da325b956feadc75fee1f4c5856b812248f2
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-04-17 08:22:26 -0700

    moving off l10n service from okra into new host

---
+++ dist/profile/manifests/docker/run_tombstone.pp
@@ -0,0 +1,16 @@
+#
+# A define that cleans up the left over from docker::run
+#
+define profile::docker::run_tombstone {
+
+  $initscript = "/etc/init/docker-${title}.conf"
+
+  file { $initscript:
+    ensure  => absent,
+  }
+
+  service { "docker-${title}":
+    ensure     => absent,
+  }
+}
+
