b954203db2072cf9cb4b4ab68131c5d6b005ca58
R. Tyler Croy <tyler@monkeypox.org>
2016-04-17 13:30:43 -0700

    Add some parameters for hiera to fill in for mirrorbrain

--- dist/profile/manifests/mirrorbrain.pp
+++ dist/profile/manifests/mirrorbrain.pp
@@ -1,6 +1,12 @@
 #
 # Configure the mirrorbrain service
-class profile::mirrorbrain {
+class profile::mirrorbrain(
+  $pg_host     = 'localhost',
+  $pg_database = 'mirrorbrain',
+  $pg_username = 'mirrorbrain',
+  $pg_password = 'mirrorbrain',
+
+) {
   include profile::firewall
   include ::mirrorbrain
   include ::mirrorbrain::apache
