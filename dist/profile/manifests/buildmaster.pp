aaa33e3f17afaf244f5d1c5a19b33645f4b51da5
R. Tyler Croy <tyler@monkeypox.org>
2016-04-18 10:50:19 -0700

    Move firewall rules for a jenkins master into the buildmaster profile

--- dist/profile/manifests/buildmaster.pp
+++ dist/profile/manifests/buildmaster.pp
@@ -1,6 +1,8 @@
 #
 # Profile for configuring the bare necessities to running a Jenkins master
 class profile::buildmaster {
+  include profile::firewall
+
   class { '::jenkins':
     lts       => true,
     executors => 0,
--- dist/role/manifests/jenkins/master.pp
+++ dist/role/manifests/jenkins/master.pp
@@ -3,4 +3,30 @@
 class role::jenkins::master {
   include profile::base
   include profile::buildmaster
+
+  # This is a legacy role imported from infra-puppet, thus the goofy numbering
+  firewall { '108 Jenkins CLI port' :
+    proto  => 'tcp',
+    port   => 47278,
+    action => 'accept',
+  }
+
+  firewall { '801 Allow Jenkins web access only on localhost':
+    proto   => 'tcp',
+    port    => 8080,
+    action  => 'accept',
+    iniface => 'lo',
+  }
+
+  firewall { '802 Block external Jenkins web access':
+    proto  => 'tcp',
+    port   => 8080,
+    action => 'drop',
+  }
+
+  firewall { '810 Jenkins CLI SSH':
+    proto  => 'tcp',
+    port   => 22222,
+    action => 'accept',
+  }
 }
