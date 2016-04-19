58b7795b700896ba96bcbf173b9d190ebf3cb6bc
R. Tyler Croy <tyler@monkeypox.org>
2016-04-19 09:38:55 -0700

    Modify the mirrorbrain role to include the update site
    It's a pretty fat role as far as our infrastructure goes, but these three
    profiles all resolve around the same /srv/jenkins/releases tree :(

--- spec/classes/role/mirrorbrain_spec.rb
+++ spec/classes/role/mirrorbrain_spec.rb
@@ -3,4 +3,6 @@ require 'spec_helper'
 describe 'role::mirrorbrain' do
   it { should contain_class 'profile::base' }
   it { should contain_class 'profile::mirrorbrain' }
+  it { should contain_class 'profile::pkgrepo' }
+  it { should contain_class 'profile::updatesite' }
 end
