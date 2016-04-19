fbb6dfa520964439916bd207409436af9ca64bec
R. Tyler Croy <tyler@monkeypox.org>
2016-04-19 08:19:55 -0700

    Add a simple updates.jenkins.io virtual host

---
+++ spec/classes/profile/updatesite_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+
+describe 'profile::updatesite' do
+  let(:fqdn) { 'updates.jenkins.io' }
+
+  it { should contain_class 'profile::updatesite' }
+  it { should contain_class 'profile::firewall' }
+
+  context 'apache setup' do
+    it { should contain_class 'apache' }
+    it { should contain_class 'profile::apachemisc' }
+    it { should contain_class 'profile::letsencrypt' }
+
+    context 'virtual hosts' do
+      it 'should contain a vhost with ssl' do
+        expect(subject).to contain_apache__vhost(fqdn).with({
+          :servername => fqdn,
+          :port => 443,
+          :docroot => "/var/www/#{fqdn}",
+        })
+      end
+
+      it 'should contain a vhost that promotes non-SSL to SSL' do
+        expect(subject).to contain_apache__vhost("#{fqdn} unsecured").with({
+          :servername => fqdn,
+          :port => 80,
+          :redirect_status => 'permanent',
+          :redirect_dest => "https://#{fqdn}/",
+        })
+      end
+    end
+  end
+
+  context 'when running in production' do
+    let(:facts) do
+      {
+        :environment => 'production',
+      }
+    end
+
+    it { should contain_letsencrypt__certonly(fqdn) }
+
+    it 'should configure the letsencrypt ssl keys on the vhost' do
+      expect(subject).to contain_apache__vhost(fqdn).with({
+        :servername => fqdn,
+        :port => 443,
+        :ssl_key => "/etc/letsencrypt/live/#{fqdn}/privkey.pem",
+        :ssl_cert => "/etc/letsencrypt/live/#{fqdn}/cert.pem",
+        :ssl_chain => "/etc/letsencrypt/live/#{fqdn}/chain.pem",
+      })
+    end
+  end
+end
