6ec85a3bb65f4bcf57d87456f5be1884fed9db9c
R. Tyler Croy <tyler@monkeypox.org>
2016-05-05 16:18:26 -0700

    Update the demo installation to Jenkins 2.1

--- dist/profile/manifests/demo.pp
+++ dist/profile/manifests/demo.pp
@@ -1,7 +1,7 @@
 #
 # Run a demo instance of Jenkins in a Docker container
 class profile::demo(
-$image_tag = '2.0-alpha-2',
+$image_tag = '2.1'
 ) {
   include profile::docker
   include profile::apachemisc
