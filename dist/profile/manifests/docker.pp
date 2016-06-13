844d670993106f00b836c1eaaf5eb87c61de6e9c
R. Tyler Croy <tyler@monkeypox.org>
2016-06-13 16:15:23 -0700

    Default to using the aufs storage driver, which is not garbage
    Unlike devicemapper, which is total garbage. Seeing failures in newer ci.j.io
    and trusted.ci.j.io nodes where `docker push` commands are failing

--- dist/profile/manifests/docker.pp
+++ dist/profile/manifests/docker.pp
@@ -2,10 +2,11 @@
 # Profile for managing basics of docker installation/configuration
 class profile::docker {
   class { '::docker':
-    version       => '1.9.1',
+    version          => '1.9.1',
     # Disabling the management of the kernel, since we have to pre-install
     # kernel modules on Ubuntu 12.04 LTS and restart the host machine anyways
-    manage_kernel => false,
+    manage_kernel    => false,
+    extra_parameters => '--storage-driver=aufs',
   }
 
   include datadog_agent::integrations::docker
