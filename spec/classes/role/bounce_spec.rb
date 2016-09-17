9887b850113974766a37a91ce115102482638eea
R. Tyler Croy <tyler@monkeypox.org>
2016-09-17 14:03:59 -0700

    Add Oliver's account to the bounce host via Hiera
    Fixes INFRA-909

--- spec/classes/role/bounce_spec.rb
+++ spec/classes/role/bounce_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe 'role::bounce' do
+  let(:facts) do
+    {
+      :hiera_role => 'bounce',
+    }
+  end
+
   it { should contain_class 'role::bounce' }
   it { should contain_class 'profile::base' }
+
+  # https://issues.jenkins-ci.org/browse/INFRA-909
+  it { should contain_user 'ogondza' }
 end
