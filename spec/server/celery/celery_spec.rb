cb0b9a1bd07f2d4ef175a90b81afe81e3ba926ac
R. Tyler Croy <tyler@monkeypox.org>
2016-09-17 17:55:47 -0700

    Configure hiera deep merging, which isn't turned on by default for some reason

--- spec/server/celery/celery_spec.rb
+++ spec/server/celery/celery_spec.rb
@@ -4,4 +4,9 @@ describe 'celery' do
   it_behaves_like "a standard Linux machine"
 
   it_behaves_like "a Jenkins build slave"
+
+  # https://issues.jenkins-ci.org/browse/INFRA-909
+  describe user('ogondza') do
+    it { should exist }
+  end
 end
