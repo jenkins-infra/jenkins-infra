70d0f9daa3ace8bea31d35c5b3f1e5862d1988b0
R. Tyler Croy <tyler@monkeypox.org>
2016-09-29 12:38:26 -0700

    Correct the comment in profile::docker
    The aufs module is still not in the default images, so we must manage
    linux-image-extra separately

--- dist/profile/manifests/docker.pp
+++ dist/profile/manifests/docker.pp
@@ -3,7 +3,7 @@
 class profile::docker {
   class { '::docker':
     # Disabling the management of the kernel, since we have to pre-install
-    # kernel modules on Ubuntu 12.04 LTS and restart the host machine anyways
+    # kernel modules on Ubuntu 14.04 LTS and restart the host machine anyways
     manage_kernel    => false,
     extra_parameters => '--storage-driver=aufs',
     require          => Package['linux-image-extra'],
