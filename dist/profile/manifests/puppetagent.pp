96072d946233dd33ef66f68c36001b44e8650641
olblak <olivier@vernin.me>
2016-12-12 15:39:38 -0800

    Add Datadog monitoring for puppet agent process
      * Add a new puppet manifest for puppetagent
      * Add puppetagent dependency to base manifest
      * Add datadog process check file
      * Add spec file for puppetagent manifest

---
+++ dist/profile/manifests/puppetagent.pp
@@ -0,0 +1,8 @@
+class profile::puppetagent {
+
+  # Add datadog check to monitor puppet agent process
+  profile::datadog_check { 'puppetagent-process-check':
+    checker => 'process',
+    source  => 'puppet:///modules/profile/puppetagent/process_check.yaml',
+  }
+}
