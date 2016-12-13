65aa28b745af874ce95af15f4e040a48957e7db3
olblak <olivier@vernin.me>
2016-12-12 16:06:04 -0800

    Spec - Fix puppetagent filename

--- spec/classes/profile/puppetagent_spec.rb
+++ spec/classes/profile/puppetagent_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 describe 'profile::puppetagent' do
     it { should contain_class 'profile::puppetagent' }
-    it { should contain_file ('/etc/dd-agent/conf.d/process.yaml') }
+    it { should contain_file ('/etc/dd-agent/conf.d/process_check.yaml') }
 end
