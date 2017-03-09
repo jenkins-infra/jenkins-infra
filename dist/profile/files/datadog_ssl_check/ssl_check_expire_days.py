6a8b6a4a877016e44b96a070699211823572a75d
Olivier Vernin <olivier@vernin.me>
2017-03-09 09:32:56 +0100

    User same tag format in all case

--- dist/profile/files/datadog_ssl_check/ssl_check_expire_days.py
+++ dist/profile/files/datadog_ssl_check/ssl_check_expire_days.py
@@ -11,6 +11,7 @@ class SSLCheckExpireDays(AgentCheck):
     def check(self, instance):
         metric = "ssl.expire_in_days"
         site = instance['site']
+        tag = "site:"+site
         p = subprocess.Popen("echo | openssl s_client -showcerts -servername " + site + " -connect " + site + ":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -f 2 -d\= | xargs -0 -I arg date -d arg \"+%s\"",stdout=subprocess.PIPE, shell=True)
         (output, err) = p.communicate()
         if output:
@@ -19,8 +20,6 @@ class SSLCheckExpireDays(AgentCheck):
             d1 = int(output)
             delta = d1 - d0
             days= delta/24/60/60 # convert the timestamp to days
-            tag="site:" + site # generate the tags
-#            print "metric: " + str(metric) + ", tag: " + str(tag) + ", days: " + str(days)
             self.gauge(metric, int(days), tags=[tag])
         else:
-            self.gauge(metric, -1, tags=[site])
+            self.gauge(metric, -1, tags=[tag])
