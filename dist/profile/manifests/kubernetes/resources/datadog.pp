e541c4371b1a1d78d7e870df0ccb4cb45677eef1
Olivier Vernin <olivier@vernin.me>
2017-03-15 14:55:39 +0100

    Add doc and missing include

--- dist/profile/manifests/kubernetes/resources/datadog.pp
+++ dist/profile/manifests/kubernetes/resources/datadog.pp
@@ -1,7 +1,16 @@
 # Deploy datadog resources on kubernetes cluster
+#   Class: profile::kubernetes::resources::datadog
+#
+#   This class deploy a datadog agent on each kubernetes node
+#
+#   Parameters:
+#     $apiKey:
+#       Contain datadog api key.
+#       Used in secret template
 class profile::kubernetes::resources::datadog (
     $apiKey = base64('encode', $::datadog_agent::api_key, 'strict')
   ){
+  include ::stdlib
   include profile::kubernetes::params
   require profile::kubernetes::kubectl
 
@@ -31,16 +40,6 @@ class profile::kubernetes::resources::datadog (
     subscribe   => [
       Exec['apply datadog/secret.yaml'],
       Exec['apply datadog/daemonset.yaml'],
-    ],
-  }
-  exec { 'Get datadog pods':
-    path        => ["${profile::kubernetes::params::bin}/"],
-    command     => 'kubectl get pods -l app=datadog',
-    refreshonly => true,
-    environment => ["KUBECONFIG=${profile::kubernetes::params::home}/.kube/config"] ,
-    logoutput   => true,
-    subscribe   => [
-      Exec['apply datadog/secret.yaml'],
     ],
   }
 }
