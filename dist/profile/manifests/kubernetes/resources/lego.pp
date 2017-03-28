41fc3b8cf237cf6f51a4089f68190803b673d061
Olivier Vernin <olivier@vernin.me>
2017-03-28 11:57:57 +0200

    Add Lego for kubernetes cluster
    	* Add kubernetes configuration files for kubernetes/lego
    	* Add tests for lego

---
+++ dist/profile/manifests/kubernetes/resources/lego.pp
@@ -0,0 +1,48 @@
+#   Class: profile::kubernetes::resources::lego
+#
+#   This class install kube lego
+#   => https://github.com/jetstack/kube-lego
+#
+#   Parameters:
+#     $email:
+#       Mail address used by letsencrypt to send notifications 
+#     $url:
+#       Endpoint used by letsencrypt to get certificate 
+#       This value can also be set to https://acme-staging.api.letsencrypt.org/directory
+#       for staging environment
+#
+#
+class profile::kubernetes::resources::lego (
+    String $email = 'infra@lists.jenkins-ci.org',
+    String $url = 'https://acme-v01.api.letsencrypt.org/directory'
+  ){
+  include profile::kubernetes::params
+  require profile::kubernetes::kubectl
+
+  file { "${profile::kubernetes::params::resources}/lego":
+    ensure => 'directory',
+    owner  => $profile::kubernetes::params::user,
+  }
+
+  profile::kubernetes::apply { 'lego/namespace.yaml':}
+
+  profile::kubernetes::apply { 'lego/configmap.yaml':
+    parameters => {
+      'email' => $email,
+      'url'   => $url
+    }
+  }
+
+  profile::kubernetes::apply { 'lego/deployment.yaml':}
+
+  # As configmap changes do not trigger pods update,
+  # we must reload pods 'manually' to use the newly updated configmap
+  exec { 'Reload lego pods':
+    path        => ["${profile::kubernetes::params::bin}/"],
+    command     => 'kubectl delete pods -l app=kube-lego',
+    refreshonly => true,
+    environment => ["KUBECONFIG=${profile::kubernetes::params::home}/.kube/config"] ,
+    logoutput   => true,
+    subscribe   => Exec['apply lego/configmap.yaml']
+  }
+}
---
+++ spec/classes/profile/kubernetes/lego_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'profile::kubernetes::resources::lego' do
+   let (:params) do 
+       {
+          :email  => 'infra@lists.jenkins-ci.org',
+          :url    => 'https://acme-v01.api.letsencrypt.org/directory'
+       }
+   end
+   it { should contain_class('profile::kubernetes::params') }
+   it { should contain_class('profile::kubernetes::kubectl') }
+   it { should contain_file("/home/k8s/resources/lego").with(
+     :ensure => 'directory',
+     :owner  => 'k8s'
+     )
+   }
+   it { should contain_profile__kubernetes__apply('lego/daemonset.yaml')}
+   it { should contain_profile__kubernetes__apply('lego/namespace.yaml')}
+   it { should contain_profile__kubernetes__apply('lego/configmap.yaml').with(
+     :parameters => { 
+        'email' => 'infra@lists.jenkins-ci.org',
+        'url'   => 'https://acme-v01.api.letsencrypt.org/directory'
+        }
+     )
+   }
+   it { should contain_exec('Reload lego pods')}
+end
