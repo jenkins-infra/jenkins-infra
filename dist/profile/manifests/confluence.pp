48472e520b0fecc27ca5badc30f13f157339ad46
R. Tyler Croy <tyler@monkeypox.org>
2017-04-01 11:45:50 -0700

    Update the mod_proxy settings for Confluence 6.1.0 with Collaborative Editing
    See <https://confluence.atlassian.com/confeap/using-apache-with-mod_proxy-480346967.html>

--- dist/profile/manifests/confluence.pp
+++ dist/profile/manifests/confluence.pp
@@ -62,7 +62,7 @@ class profile::confluence (
 
   docker::run { 'confluence':
     command         => undef,
-    ports           => ['8081:8080'],
+    ports           => ['8081:8080', '8091:8091'],
     image           => "jenkinsciinfra/confluence:${image_tag}",
     volumes         => ['/srv/wiki/home:/srv/wiki/home', '/srv/wiki/cache:/srv/wiki/cache'],
     env_file        => '/srv/wiki/container.env',
@@ -111,6 +111,7 @@ class profile::confluence (
     servername      => 'wiki.jenkins-ci.org',
     port            => '80',
     docroot         => '/srv/wiki/docroot',
+    access_log_pipe => '/dev/null',
     redirect_status => 'temp',
     redirect_dest   => 'https://wiki.jenkins-ci.org/'
   }
@@ -130,5 +131,12 @@ class profile::confluence (
 
   host { 'wiki.jenkins-ci.org':
     ip => '127.0.0.1',
+  }
+
+  firewall {
+    '299 allow synchrony for Confluence':
+      proto  => 'tcp',
+      port   => 8091,
+      action => 'accept',
   }
 }
