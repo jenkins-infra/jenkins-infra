1344514b5930e5b815a6d5c4767abdc9da352a11
olblak <olivier@vernin.me>
2017-06-22 16:43:01 +0200

    Fix spec test for datadog_pluginsite_check

--- spec/classes/profile/datadog_pluginsite_check.rb
+++ spec/classes/profile/datadog_pluginsite_check.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'profile::datadog_ssl_check' do
+describe 'profile::datadog_pluginsite_check' do
   it { should contain_class 'datadog_agent' }
   it { should contain_file 'plugins_api_check.py' }
   it { should contain_file 'plugins_api_check.yaml' }
