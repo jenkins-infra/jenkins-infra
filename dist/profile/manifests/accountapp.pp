edb5ac1e8bd9f4de4aa58b0a8b73072967a085d9
olblak <olivier@vernin.me>
2017-09-14 10:36:40 +0200

    Remove accountapp from eggplant

--- dist/profile/manifests/accountapp.pp
+++ dist/profile/manifests/accountapp.pp
@@ -1,113 +1,47 @@
 #
-# Profile defining the necessary resources to provision our LDAP-based
+# Profile cleaning the necessary resources to provision our LDAP-based
 # accountapp
+# Remark, following resources are not removed:
+#   - docker image: jenkinsciinfra/account-app
+#   - service file: /etc/init.d/docker-account-app
+#
 class profile::accountapp(
-  # all injected from hiera
-  $image_tag,
-  $ldap_url = 'ldap://localhost:389/',
-  $ldap_password = '',
-  $smtp_server = 'localhost',
-  $recaptcha_key = '',
-  $app_url = 'https://accounts.jenkins.io/',
-  $jira_url = 'https://issues.jenkins-ci.org/',
-  $jira_username = accountapp,
-  $jira_password = '',
+  $image_tag = '23-build5a2c1e',
 ) {
-  include ::firewall
   include profile::docker
-  include profile::letsencrypt
   include profile::apachemisc
 
   validate_string($image_tag)
-  validate_string($ldap_url)
-  validate_string($ldap_password)
-  validate_string($smtp_server)
-  validate_string($recaptcha_key)
 
-  file { '/etc/accountapp' :
-    ensure => directory,
-    # Don't allow anything not declared in Puppet to be dropped in there
-    purge  => true,
-  }
+  $vhosts = [
+    'accounts.jenkins.io',
+    'accounts.jenkins.io unsecured'
+    ]
 
-  file { '/etc/accountapp/config.properties':
-    ensure  => file,
-    content => template("${module_name}/accountapp/config.properties.erb"),
-    require => File['/etc/accountapp'],
-  }
+  $files = [
+    '/etc/accountapp',
+    '/etc/letsencrypt/live/accounts.jenkins.io/privkey.pem',
+    '/etc/letsencrypt/live/accounts.jenkins.io/cert.pem',
+    '/etc/letsencrypt/live/accounts.jenkins.io/chain.pem',
+    ]
 
-  docker::image { 'jenkinsciinfra/account-app':
-    image_tag => $image_tag,
-  }
-
+  # Remark this only delete docker container but do not remove the init.d service
   docker::run { 'account-app':
-    command          => undef,
-    image            => "jenkinsciinfra/account-app:${image_tag}",
-    volumes          => ['/etc/accountapp:/etc/accountapp'],
-    require          => File['/etc/accountapp/config.properties'],
-    env              => [
-      "LDAP_URL=${ldap_url}",
-      "LDAP_PASSWORD=${ldap_password}",
-      "JIRA_URL=${jira_url}",
-      "JIRA_USERNAME=${jira_username}",
-      "JIRA_PASSWORD=${jira_password}",
-    ],
-    extra_parameters => ['--net=host'],
+    ensure => 'absent',
+    image  => "jenkinsciinfra/account-app:${image_tag}",
   }
 
-  profile::datadog_check { 'accountapp-http-check':
-    ensure  => 'absent',
-    checker => 'http_check',
-    source  => 'puppet:///modules/profile/accountapp/http_check.yaml',
-  }
-
-
-  # docroot is required for apache::vhost but should never be used because
-  # we're proxying everything here
-  $docroot = '/var/www/html'
-
-  apache::vhost { 'accounts.jenkins.io':
-    serveraliases => [
-      'accounts.jenkins-ci.org',
-    ],
-    port          => '443',
-    ssl           => true,
-    docroot       => $docroot,
-    proxy_pass    => [
-      {
-        path         => '/',
-        url          => 'http://localhost:8080/',
-        reverse_urls => 'http://localhost:8080/',
-      },
-    ],
-  }
-
-  apache::vhost { 'accounts.jenkins.io unsecured':
-    servername      => 'accounts.jenkins.io',
-    serveraliases   => [
-      'accounts.jenkins-ci.org',
-    ],
-    port            => '80',
-    docroot         => $docroot,
-    redirect_status => 'permanent',
-    redirect_dest   => $app_url,
-  }
-
-  # We can only acquire certs in production due to the way the letsencrypt
-  # challenge process works
-  if (($::environment == 'production') and ($::vagrant != '1')) {
-    letsencrypt::certonly { 'accounts.jenkins.io':
-        domains     => ['accounts.jenkins.io', 'accounts.jenkins-ci.org'],
-        plugin      => 'apache',
-        manage_cron => true,
+  $vhosts.each | String $vhost| {
+    apache::vhost { $vhost:
+      ensure  => 'absent',
+      docroot => '/var/www/html/',
     }
+  }
 
-    Apache::Vhost <| title == 'accounts.jenkins.io' |> {
-      ssl_key       => '/etc/letsencrypt/live/accounts.jenkins.io/privkey.pem',
-      # When Apache is upgraded to >= 2.4.8 this should be changed to
-      # fullchain.pem
-      ssl_cert      => '/etc/letsencrypt/live/accounts.jenkins.io/cert.pem',
-      ssl_chain     => '/etc/letsencrypt/live/accounts.jenkins.io/chain.pem',
+  $files.each | String $file| {
+    file { $file:
+      ensure => 'absent',
+      force  => true
     }
   }
 }
