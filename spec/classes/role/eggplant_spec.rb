96dfc48bcdc9f94e610c60dfaf118071ea976a75
R. Tyler Croy <tyler@monkeypox.org>
2017-11-21 16:12:47 -0800

    Remove the staticsite profile

--- spec/classes/role/eggplant_spec.rb
+++ spec/classes/role/eggplant_spec.rb
@@ -5,6 +5,5 @@ describe 'role::eggplant' do
 
   it { should contain_class 'profile::apachemisc' }
   it { should contain_class 'profile::sudo::osu' }
-  it { should contain_class 'profile::staticsite' }
   it { should contain_class 'profile::accountapp' }
 end
