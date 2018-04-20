274924905391e215bf780aeafa8e533202cc88cb
olblak <me@olblak.com>
2018-04-20 15:27:23 +0200

    Update compliance rspec test

--- spec/classes/profile/compliance_spec.rb
+++ spec/classes/profile/compliance_spec.rb
@@ -4,9 +4,9 @@ describe 'profile::compliance' do
   it { should contain_class 'profile::compliance' }
 
   context 'libssl1.0.0.' do
-    it 'should comply with USN-2959-1' do
+    it 'should comply with USN-3628-1' do
       expect(subject).to contain_package('libssl1.0.0').with({
-        :ensure => '1.0.1f-1ubuntu2.24',
+        :ensure => '1.0.1f-1ubuntu2.25',
       })
     end
   end
