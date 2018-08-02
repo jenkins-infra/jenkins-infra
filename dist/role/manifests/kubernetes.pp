98054537d416a5eab31607eceab9fbf2a18519b6
olblak <me@olblak.com>
2018-08-02 16:13:53 +0200

    Add fallback update center

--- dist/role/manifests/kubernetes.pp
+++ dist/role/manifests/kubernetes.pp
@@ -3,6 +3,7 @@ class role::kubernetes{
   include profile::base
   include profile::kubernetes::resources::datadog
   include profile::kubernetes::resources::reports
+  include profile::kubernetes::resources::updates_proxy
   include profile::kubernetes::resources::javadoc
   include profile::kubernetes::resources::pluginsite
   include profile::kubernetes::resources::kube_state_metrics
