5701f339fbca35f6ebf68e7fdbe54ae98687d511
Olblak <me@olblak.com>
2019-02-04 20:30:32 +0100

    Add batch-upload script to upload released artifacts

---
+++ dist/profile/files/mirrorbrain/batch-upload.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source /srv/releases/.azure-storage-env
+
+## Sync files from "/srv/releases/jenkins/$CONTAINER" to $CONTAINER for each container in "$AZURE_STORAGE_ACCOUNT",
+for CONTAINER in $(az storage container list --account-name "$AZURE_STORAGE_ACCOUNT" --account-key "$AZURE_STORAGE_KEY" --query '[*].name' --output table ); do 
+        if [ -d "/srv/releases/jenkins/$CONTAINER" ]; then
+                echo "Syncing Container: $CONTAINER";
+
+                time blobxfer upload \
+                        --local-path "/srv/releases/jenkins/$CONTAINER/" \
+                        --storage-account-key "$AZURE_STORAGE_KEY" \
+                        --storage-account "$AZURE_STORAGE_ACCOUNT" \
+                        --remote-path "$CONTAINER" \
+                        --recursive \
+                        --skip-on-lmt-ge \
+                        --connect-timeout 30 \
+                        --exclude '.htaccess' 
+        fi
+done;
