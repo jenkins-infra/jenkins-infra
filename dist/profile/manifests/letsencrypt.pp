8db3c1f43f66f8fd5b21ffb106ecb8bf918e9dea
Olblak <me@olblak.com>
2019-02-15 16:14:53 +0100

    Install python-certbot-apache for letsencrypt plugin

--- dist/profile/manifests/letsencrypt.pp
+++ dist/profile/manifests/letsencrypt.pp
@@ -8,6 +8,10 @@ class profile::letsencrypt {
     }
   }
 
+  package { 'python-certbot-apache':
+    ensure => present
+  }
+
   cron { 'letsencrypt-renew-reload':
     ensure  => present,
     command => '/opt/letsencrypt/letsencrypt-auto renew --quiet --renew-hook="service apache2 reload"',
