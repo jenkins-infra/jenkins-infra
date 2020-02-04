11ccb533ed5ff13064f6734e62ff8d228ce86f01
Gavin Mogan <git@gavinmogan.com>
2020-02-03 16:03:29 -0800

    Find statement returns 2 colums, 1 for sorting (date) 1 for using (filename). Then often files are gz, so use zcat

--- dist/profile/files/confluence/report_last_log.sh
+++ dist/profile/files/confluence/report_last_log.sh
@@ -2,13 +2,15 @@
 # make sure all can read this file
 umask 100
 # grab all log files but sort by date
-find /var/log/apache2/wiki.jenkins-ci.org -name 'access.log*' -type f -printf "%T+\t%p\n" | sort | \
+find /var/log/apache2/wiki.jenkins-ci.org -name 'access.log*' -type f -printf "%T+\t%p\n" | sort -nk1 | \
   # grab the two newest
   tail -n 2 | \
   # then grab the second newest (so one day ago)
   head -n 1 | \
+  # grab the actual filename
+  awk -F"\t" '{print $2}' | \
   # cat it
-  xargs cat | \
+  xargs zcat | \
   # url field
   awk -F" " '$9 == "200" { print $7 }' | \
   # remove the blank lines
