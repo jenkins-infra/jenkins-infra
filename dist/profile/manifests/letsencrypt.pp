7f2e4e8d1c88a440faf9564551ab199139cb65df
Olivier Vernin <olivier@vernin.me>
2021-07-02 16:24:23 +0200

    Fix indent
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- dist/profile/manifests/letsencrypt.pp
+++ dist/profile/manifests/letsencrypt.pp
@@ -2,9 +2,9 @@
 # This profile configures letsencrypt on the host it's applied to
 class profile::letsencrypt {
   class { '::letsencrypt':
-    config => {
-        email  => lookup('letsencrypt::config::email'),
-        server => lookup('letsencrypt::config::server'),
+    config            => {
+      email  => lookup('letsencrypt::config::email'),
+      server => lookup('letsencrypt::config::server'),
     },
     renew_cron_ensure => 'present',
   }
