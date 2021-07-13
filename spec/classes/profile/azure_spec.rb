18ed6678466e52dd50c29d4cbfdd18edd371dcad
Olivier Vernin <olivier@vernin.me>
2021-07-13 14:40:25 +0200

    Cleanup azure-cli rspec test
    Signed-off-by: Olivier Vernin <olivier@vernin.me>

--- spec/classes/profile/azure_spec.rb
+++ spec/classes/profile/azure_spec.rb
@@ -10,17 +10,6 @@ describe 'profile::azure' do
       }
     end
 
-    context 'system requirements' do
-      it { should contain_package 'python-pip' }
-    end
-
-    it 'should remove the azure-cli from pip' do
-      expect(subject).to contain_package('azure-cli-python').with({
-        :provider => :pip,
-        :ensure => :absent,
-      })
-    end
-
     it { should contain_apt__source('azure-cli') }
     it { should contain_package('azure-cli') }
   end
