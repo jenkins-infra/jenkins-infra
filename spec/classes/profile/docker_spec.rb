10cdca7732789a39c4d75b74e9fea0efd93b3b52
Damien Duportal <damien.duportal@gmail.com>
2021-07-27 13:01:42 +0200

    fix: correct docker role when using it the first time on a scratch machine
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/docker_spec.rb
+++ spec/classes/profile/docker_spec.rb
@@ -4,4 +4,6 @@ describe 'profile::docker' do
   it { should contain_class 'docker' }
   it { should contain_class 'datadog_agent::integrations::docker_daemon' }
   it { should contain_firewall('010 allow inter-docker traffic').with_action('accept').with_iniface('docker0') }
+  it { should contain_file('/etc/docker').with('ensure' => 'directory')}
+  it { should contain_file('/etc/docker/daemon.json')}
 end
