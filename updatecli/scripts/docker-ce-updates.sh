ebf3af0781dd537ef21913c112ae0cc2cf7fd428
Antoine Neveux <aneveux@cloudbees.com>
2021-11-03 22:05:00 +0100

    [INFRA-3074] Modify script to directly run in a docker container

--- updatecli/scripts/docker-ce-updates.sh
+++ updatecli/scripts/docker-ce-updates.sh
@@ -1,30 +1,13 @@
 #!/bin/bash
-# This script uses apt to find the latest version of docker-ce available.
 set -eu -o pipefail
 
-for cli in apt-get apt-cache grep cut xargs
-do
-  if ! command -v $cli >/dev/null 2>&1
-  then
-    echo "ERROR: command line ${cli} required but not found. Exiting."
-    exit 1
-  fi
-done
+if ! command -v docker >/dev/null 2>&1
+then
+  echo "ERROR: command line 'docker' required but not found. Exiting."
+  exit 1
+fi
 
-# Updating the list of latest updates available for installed packages on the system
-apt-get update >/dev/null
+SCRIPT_PATH=$(dirname "$0")
+SCRIPT_PATH=$(cd "$SCRIPT_PATH" && pwd)
 
-# Retrieve from apt-cache the latest version of docker-ce available
-# Note: apt-cache policy will return a result like:
-# :# apt-cache policy docker-ce
-# docker-ce:
-#   Installed: (none)
-#   Candidate: (none)
-#   Version table:
-# We want to get the Candidate version (which is the latest available)
-# And we want it in a readable format
-apt-cache policy docker-ce `# 1. Retrieve information about docker-ce from apt` \
-  | grep 'Candidate' `# 2. Keep only the line about the Candidate version (latest available)` \
-  | cut -f2,3 -d':' `# 3. Cut it so we only keep the version and remove title (version contains a :, hence keeping fields 2 and 3)` \
-  | xargs `# 4. Trimming the result (removing spaces before and after)` \
-  | { read -r x ; if [ "$x" == '(none)' ]; then exit 1; else echo "'${x}'"; fi }  # 5. Failing if the result is empty or surrounding it with simple quotes ('')
+docker run --rm --name=updatecli-docker-ce --volume="$SCRIPT_PATH":/scripts/ --entrypoint=bash ubuntu:bionic -c "chmod +x /scripts/fetch-docker-ce-latest-version.sh ; /scripts/fetch-docker-ce-latest-version.sh"
