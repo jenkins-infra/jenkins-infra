9eebc8e578e098f316acdf3553c1c11e233af707
Damien Duportal <damien.duportal@gmail.com>
2021-11-29 11:14:50 +0100

    chore: removed unused kubernetes-related code
    This change removes all the puppet code (and associated files such as hieradata) from Kubernetes management.
    
    Rationale is that Kubernetes is managed outside Puppet code since 2 years:
    - Kubernetes cluster infrastructure is managed by terraform or manually (or mix of both) on jenkins-infra/azure and jenkins-infra/aws
    - Application running in Kubernetes are fully managed in jenkins-infra/charts by helmfile
    
    Side note: the profile associated to the puppetmaster node had been changed by this change. We'll have to be careful when applied.
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/kubernetes/evergreen_spec.rb
+++
@@ -1,8 +0,0 @@
-
-require 'spec_helper'
-
-describe 'profile::kubernetes::resources::evergreen' do
-    it { should contain_class 'profile::kubernetes::kubectl' }
-    it { should contain_class 'profile::kubernetes::resources::nginx' }
-    it { should contain_class 'profile::kubernetes::resources::lego' }
-end
