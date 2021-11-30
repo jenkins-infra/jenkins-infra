01887e7817c179b2a1aaf18239871a1143392922
Damien Duportal <damien.duportal@gmail.com>
2021-11-30 13:38:25 +0100

    chore: removed unused accountapp puppet code (service managed in jenkins-infra/charts)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/role/eggplant_spec.rb
+++ spec/classes/role/eggplant_spec.rb
@@ -5,5 +5,4 @@ describe 'role::eggplant' do
 
   it { should contain_class 'profile::apachemisc' }
   it { should contain_class 'profile::sudo::osu' }
-  it { should contain_class 'profile::accountapp' }
 end
--- spec/server/eggplant/eggplant_spec.rb
+++ spec/server/eggplant/eggplant_spec.rb
@@ -5,12 +5,6 @@ describe 'eggplant' do
   it_behaves_like 'a static site host'
   it_behaves_like 'a Docker host'
 
-  context 'accountapp' do
-    describe file('/etc/accountapp') do
-      it { should_not exist }
-    end
-  end
-
 
   it_behaves_like 'an Apache webserver'
 
