11db0878ec379eb687858666db7de9d38533ff38
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2022-02-14 20:01:34 +0100

    Remove obsolete security options (#2064)
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- dist/profile/files/buildmaster/agent-security.groovy
+++ dist/profile/files/buildmaster/agent-security.groovy
@@ -1,16 +1,8 @@
 #!/usr/bin/env groovy
-/*
- * Restrict the JNLP protocols to only those which should be enabled (modern
- * and secure ones)
- */
-
 import jenkins.model.*
 import org.kohsuke.stapler.StaplerProxy
 import jenkins.security.s2m.AdminWhitelistRule
 
-/* Restrict agent protocols to only modern and secure ones */
-Jenkins.instance.agentProtocols = ['JNLP4-connect', 'Ping']
-Jenkins.instance.save()
-
 /* INFRA-659: enforce  Agent -> Master Access control */
+// TODO Remove once we're on 2.326+
 Jenkins.instance.getExtensionList(StaplerProxy).get(AdminWhitelistRule).masterKillSwitch = false
