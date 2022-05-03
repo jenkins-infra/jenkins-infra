394b76a6a915b92ee1d881d2bc930c418e74fa0d
Basil Crow <me@basilcrow.com>
2022-05-03 15:57:15 -0700

    Fix incorrect import

--- dist/profile/files/buildmaster/pipeline-configuration.groovy
+++ dist/profile/files/buildmaster/pipeline-configuration.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-import org.jenkinsci.plugins.pipeline.modeldefinition.config.GlobalConfig
+import org.jenkinsci.plugins.docker.workflow.declarative.GlobalConfig
 
 /* Set the default Docker label for Declarative Pipeline to .. wait for it ..
  * docker
