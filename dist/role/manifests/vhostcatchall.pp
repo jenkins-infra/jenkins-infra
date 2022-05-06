fdcaeb996888ffad1900967e01b17b9864ef72de
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2022-05-06 12:21:23 +0200

    feat(catchall): remove (#2158)

--- dist/role/manifests/vhostcatchall.pp
+++
@@ -1,9 +0,0 @@
-#
-# This role is for a simple apache host which serves as a catch-all for legacy
-# domains under jenkins-ci.org
-#
-# <https://issues.jenkins-ci.org/browse/INFRA-639>
-class role::vhostcatchall {
-  include profile::base
-  include profile::catchall
-}
