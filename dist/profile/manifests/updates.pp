94cfc0e773cc8c511eb23784032cdc8d215a7e19
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2022-07-18 15:05:30 +0200

    Update dist/profile/manifests/updates.pp
    Co-authored-by: Damien Duportal <damien.duportal@gmail.com>

--- dist/profile/manifests/updates.pp
+++ dist/profile/manifests/updates.pp
@@ -2,7 +2,6 @@
 # Defines an "updates.jenkins.io" server for serving updatesite, aims to deal with volume management
 #
 class profile::updates() {
-  include stdlib
   # volume configuration is in hiera
   include lvm
 
