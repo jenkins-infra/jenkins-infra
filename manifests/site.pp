20cb0e2326888bc82b09cceaf029452fd9c6473b
smerle33 <smerle@cloudbees.com>
2022-07-18 15:21:37 +0200

    remove unnecessary profile

--- manifests/site.pp
+++ manifests/site.pp
@@ -70,6 +70,11 @@ node 'pkg' {
 
 node 'oracle.updates.jenkins.io' {
   sshkeyman::hostkey { ['oracle.updates.jenkins.io']: }
+  # volume configuration is in hiera
+  include lvm
+  package { 'lvm2':
+    ensure => present,
+  }
   include role::updates
 }
 
