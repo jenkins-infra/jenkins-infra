5a54f7e662f7dbb186ece6f9ee4ee94a90dd640b
Damien Duportal <damien.duportal@gmail.com>
2022-08-13 14:27:53 +0200

    cleanup: remove unused items (#2326)
    * cleanup: remove unused and empty hieradata for tomato
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup(demo) remove unused profile demo
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup(r10k) remove unused  template files. Amends 5c4210e34c6b047e7a6bdebaca2b9e2ea7eabe2c
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup(profiles) remove unused templates and files
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup: remove unused profile nolegacy
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup: remove unused profile jenkinsgroovy
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup: remove unused profile apachemaintenance
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup: remove unused profile tombstone
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup: remove unused profile atlassian and related hieradata items
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * chore(gems) use bundle install without custom local path
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    (cherry picked from commit 048450c722daf2ab5e04ad9f848366a320cd0903)
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/nolegacy_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'profile::nolegacy' do
-  it { expect(subject).to contain_cron('pull puppet updates').with_ensure('absent') }
-  it { expect(subject).to contain_cron('clean up old puppet logs').with_ensure('absent') }
-  it { expect(subject).to contain_file('/root/infra-puppet').with_ensure('absent') }
-  it { expect(subject).to contain_cron('clean the repo-update cache').with_ensure('absent') }
-end
