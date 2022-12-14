50800afdb5e8afd1551ae81cf5b8a16174a4a0aa
Hervé Le Meur <hlemeur@cloudbees.com>
2022-12-14 15:29:35 +0100

    add comment and put in bottom

--- spec/classes/profile/openvpn_spec.rb
+++ spec/classes/profile/openvpn_spec.rb
@@ -2,6 +2,27 @@ require 'spec_helper'
 
 describe 'profile::openvpn' do
 
+  it { expect(subject).to contain_class 'stdlib' }
+  it { expect(subject).to contain_class 'profile::docker' }
+
+  it { expect(subject).to contain_package 'net-tools' }
+
+  it { expect(subject).to contain_firewall '107 accept incoming 443 connections' }
+
+  # Routing from VPN networks to eth0 network
+  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 192.168.0.0/24 on ports 80/443" } #.with('outiface' => 'eth0') }
+  it { expect(subject).to contain_firewall "100 allow routing from 172.19.0.0/24 to 192.168.0.0/24 on ports 80/443" }
+
+  # Routing from VPN networks to eth1 network
+  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 192.168.100.0/24 on ports 80/443" }
+  it { expect(subject).to contain_firewall "100 allow routing from 172.19.0.0/24 to 192.168.100.0/24 on ports 80/443" }
+
+  # Routing from VPN networks to eth1 peered networks
+  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 10.0.0.0/16 on ports 80/443" }
+  it { expect(subject).to contain_firewall "100 allow routing from 172.19.0.0/24 to 10.0.0.0/16 on ports 80/443" }
+  it { expect(subject).to contain_exec "addroute 10.0.0.0 through 192.168.100.1 (NIC eth1)" }
+
+  # Disable network config in cloud-init if there is more than two network interfaces
   context 'with 3 network interfaces' do
     let(:facts) do
       {
@@ -27,24 +48,4 @@ describe 'profile::openvpn' do
       :rspec_hieradata_fixture => 'profile_openvpn',
     }
   end
-
-  it { expect(subject).to contain_class 'stdlib' }
-  it { expect(subject).to contain_class 'profile::docker' }
-
-  it { expect(subject).to contain_package 'net-tools' }
-
-  it { expect(subject).to contain_firewall '107 accept incoming 443 connections' }
-
-  # Routing from VPN networks to eth0 network
-  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 192.168.0.0/24 on ports 80/443" } #.with('outiface' => 'eth0') }
-  it { expect(subject).to contain_firewall "100 allow routing from 172.19.0.0/24 to 192.168.0.0/24 on ports 80/443" }
-
-  # Routing from VPN networks to eth1 network
-  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 192.168.100.0/24 on ports 80/443" }
-  it { expect(subject).to contain_firewall "100 allow routing from 172.19.0.0/24 to 192.168.100.0/24 on ports 80/443" }
-
-  # Routing from VPN networks to eth1 peered networks
-  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 10.0.0.0/16 on ports 80/443" }
-  it { expect(subject).to contain_firewall "100 allow routing from 172.19.0.0/24 to 10.0.0.0/16 on ports 80/443" }
-  it { expect(subject).to contain_exec "addroute 10.0.0.0 through 192.168.100.1 (NIC eth1)" }
 end
