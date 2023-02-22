615a3f05d2242821512ec7545d13ba49361ac4cb
Damien Duportal <damien.duportal@gmail.com>
2023-02-22 18:49:07 +0100

    fix(rng) switch to the rng-tools5 package to ensure all ubuntu versions are covered
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- dist/profile/manifests/rngd.pp
+++ dist/profile/manifests/rngd.pp
@@ -1,8 +1,12 @@
 # Profile for managing rngd packet installation
 class profile::rngd {
-  ['rng-tools'].each | $package | {
-    package { $package:
-      ensure => present,
-    }
+  # This package exists on ubuntu 18.04, 20.04 and 22.04
+  package { 'rng-tools5':
+    ensure => present,
+  }
+
+  # This package is the "legacy" on ubuntu 18.04 and 20.04, but is renamed as 'rng-tools-debian' on 22.04+
+  package { 'rng-tools':
+    ensure => absent,
   }
 }
