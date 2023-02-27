195f59824d380a91576056fa4449dcc8d501ccc9
Damien Duportal <damien.duportal@gmail.com>
2023-02-27 16:44:29 +0100

    fix(batch upload,sync) follow up of 503b7ac to use a virtual env for blobxfer CLI
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- batch-upload.bash
+++ batch-upload.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+source /srv/releases/.venv-blobxfer/bin/activate
 source /srv/releases/.azure-storage-env
 
 ## Sync files from "/srv/releases/jenkins/$CONTAINER" to $CONTAINER for each container in "$AZURE_STORAGE_ACCOUNT",
-for CONTAINER in $(az storage container list --account-name "$AZURE_STORAGE_ACCOUNT" --account-key "$AZURE_STORAGE_KEY" --query '[*].name' --output table ); do 
+for CONTAINER in $(az storage container list --account-name "$AZURE_STORAGE_ACCOUNT" --account-key "$AZURE_STORAGE_KEY" --query '[*].name' --output table ); do
 	if [ -d "/srv/releases/jenkins/$CONTAINER" ]; then
 		echo "Syncing Container: $CONTAINER";
 
