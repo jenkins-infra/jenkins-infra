e604756edc6a2acbb200e08d8b0d2ef2c0490197
Damien Duportal <damien.duportal@gmail.com>
2023-06-24 11:26:45 +0200

    fix(openvpn) add ports 22 and 5432 to iptables nat rules + ensure default forward policy is accept (#2922)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/openvpn_spec.rb
+++ spec/classes/profile/openvpn_spec.rb
@@ -13,12 +13,17 @@ describe 'profile::openvpn' do
   it { expect(subject).to contain_package 'net-tools' }
 
   it { expect(subject).to contain_firewall '107 accept incoming 443 connections' }
+  it { expect(subject).to contain_firewall '107 accept incoming 22 connections' }
+  it { expect(subject).to contain_firewallchain('FORWARD:filter:IPv4').with(
+    :ensure => 'present',
+    :policy => 'accept',
+  )}
 
   # Routing from VPN networks to eth1 network
-  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 192.168.100.0/24 on ports 80/443" }
+  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 192.168.100.0/24 on ports 22/80/443/5432" }
 
   # Routing from VPN networks to eth1 peered networks
-  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 10.0.0.0/16 on ports 80/443" }
+  it { expect(subject).to contain_firewall "100 allow routing from 127.0.10.0/24 to 10.0.0.0/16 on ports 22/80/443/5432" }
   it { expect(subject).to contain_exec "addroute 10.0.0.0 through 192.168.100.1 (NIC eth1)" }
 
   # Disable network config in cloud-init if there is more than two network interfaces
