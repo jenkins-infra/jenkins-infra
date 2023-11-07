6438eae2e36f8537e9a76396896d023ec61b4cce
Damien Duportal <damien.duportal@gmail.com>
2023-11-07 13:27:40 +0100

    cleanup: remove users who do not access the infra anymore (#2947)
    * cleanup: remove users who do not access the infra anymore
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * feat(usage.jenkins.io) allow abayer
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/accounts_spec.rb
+++ spec/classes/profile/accounts_spec.rb
@@ -3,5 +3,8 @@ require 'spec_helper'
 describe 'profile::accounts' do
   it { expect(subject).to contain_account 'tyler' }
   it { expect(subject).to contain_account 'kohsuke' }
-  it { expect(subject).to contain_account 'abayer' }
+  it { expect(subject).to contain_account 'dduportal' }
+  it { expect(subject).to contain_account 'smerle' }
+  it { expect(subject).to contain_account 'hlemeur' }
+  it { expect(subject).to contain_account 'timja' }
 end
