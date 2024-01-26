8db9f67dd6985b2b4f058446eeca939981857de9
Damien Duportal <damien.duportal@gmail.com>
2024-01-26 09:08:20 +0100

    cleanup: remove unused scripts
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- azure-sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-eval `cat /srv/releases/.azure-storage-env`
-wget -O release-blob-sync https://raw.githubusercontent.com/jenkins-infra/azure/master/scripts/release-blob-sync
-/usr/bin/ruby release-blob-sync | sh
