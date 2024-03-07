433109da242e6ae49bcb321b22f2f5a012659640
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2024-03-07 16:38:33 +0100

    feat(pkg,agent): add `azure-cli` (#3322)

--- spec/classes/profile/azcopy_spec.rb
+++ spec/classes/profile/azcopy_spec.rb
@@ -5,4 +5,5 @@ describe 'profile::azcopy' do
   it { is_expected.to contain_exec('Install azcopy') }
   it { is_expected.to contain_package('curl') }
   it { is_expected.to contain_package('tar') }
+  it { is_expected.to contain_package('azure-cli') }
 end
