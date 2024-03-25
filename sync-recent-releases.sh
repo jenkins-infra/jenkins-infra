1d61ac01ecda18a06404d6833e499f74eadf8fc7
Damien Duportal <damien.duportal@gmail.com>
2024-03-25 19:19:31 +0100

    hotfix(sync-recent-releases) permissions and azcopy command syntax

--- sync-recent-releases.sh
+++ sync-recent-releases.sh
@@ -37,18 +37,22 @@ urlWithoutToken=${fileShareSignedUrl%\?*}
 token=${fileShareSignedUrl#*\?}
 
 while IFS= read -r release; do
+    # Ensure proper permissions are set (mirrorbrain is the owner and can write while www-data only need read access for webserver)
+    find "${BASE_DIR}/plugins/${release}" -exec chown mirrorbrain:www-data {} \;
+    find "${BASE_DIR}/plugins/${release}" -type f -exec chmod 640 {} \;
+    find "${BASE_DIR}/plugins/${release}" -type d -exec chmod 750 {} \;
+    
     echo "Uploading ${release}"
 
     # Don't print any trace
     set +x
 
+    # ${release} is a directory (hence the trailing slash for destination) to avoid 
     azcopy sync \
         --skip-version-check \
         --recursive=true \
         --delete-destination=false \
-        --compare-hash=MD5 \
-        --put-md5 \
-        "${BASE_DIR}/plugins/${release}" "${urlWithoutToken}plugins/${release}?${token}"
+        "${BASE_DIR}/plugins/${release}" "${urlWithoutToken}plugins/?${token}"
 
     # Following commands traces are safe
     set -x
@@ -67,5 +71,6 @@ set -x
 echo ">> Telling OSUOSL to gets the new bits"
 ssh jenkins@ftp-osl.osuosl.org 'sh trigger-jenkins'
 
-echo ">> Delivering bits to mirrors fallback (archives.jenkins.io) from OSUOSL"
-/srv/releases/populate-archives.sh
+## Commented out until OSUOSL <-> archives permissions are fixed
+# echo ">> Delivering bits to mirrors fallback (archives.jenkins.io) from OSUOSL"
+# /srv/releases/populate-archives.sh
