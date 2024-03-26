43981bf0ae05f62d28117b8507268acc773fb989
Damien Duportal <damien.duportal@gmail.com>
2024-03-26 16:53:23 +0100

    fixup: copy content of dir jenkins/
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- sync.sh
+++ sync.sh
@@ -93,7 +93,7 @@ if [[ "${FLAG}" = '--full-sync' ]]; then
     --overwrite=ifSourceNewer `# Only overwrite if source is more recent (time comparison)` \
     --log-level=ERROR `# Do not write too much logs (I/O...)` \
     --include-pattern='*.json' `# First quick pass on the update center JSON files` \
-    "${BASE_DIR}" "${fileShareSignedUrl}"
+    "${BASE_DIR}/*" "${fileShareSignedUrl}"
 
   azcopy copy --dry-run \
     --skip-version-check `# Do not check for new azcopy versions (we have updatecli + puppet for this)` \
@@ -101,7 +101,7 @@ if [[ "${FLAG}" = '--full-sync' ]]; then
     --overwrite=ifSourceNewer `# Only overwrite if source is more recent (time comparison)` \
     --log-level=ERROR `# Do not write too much logs (I/O...)` \
     --exclude-pattern='*.json' `# Second pass with all files except update center JSON files` \
-    "${BASE_DIR}" "${fileShareSignedUrl}"
+    "${BASE_DIR}/*" "${fileShareSignedUrl}"
 
   # Remove completed azcopy plans
   azcopy jobs clean --with-status=completed
