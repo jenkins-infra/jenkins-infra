b29f778ad670198591b4053c5cb502dfcdae449a
Damien Duportal <damien.duportal@gmail.com>
2024-08-26 15:08:54 +0200

    revoke hlemeur access
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/accounts_spec.rb
+++ spec/classes/profile/accounts_spec.rb
@@ -5,6 +5,5 @@ describe 'profile::accounts' do
   it { expect(subject).to contain_account 'kohsuke' }
   it { expect(subject).to contain_account 'dduportal' }
   it { expect(subject).to contain_account 'smerle' }
-  it { expect(subject).to contain_account 'hlemeur' }
   it { expect(subject).to contain_account 'timja' }
 end
