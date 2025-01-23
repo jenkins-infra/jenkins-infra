805ed381668d22def4484d3f4b5c61fff45f8fe3
Damien Duportal <damien.duportal@gmail.com>
2025-01-23 17:09:34 +0100

    fixup - comments
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/letsencrypt_spec.rb
+++ spec/classes/profile/letsencrypt_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 
+# TODO: track with updatecli
 certbot_version = "3.1.0"
+# TODO: track with updatecli
 certbot_dns_azure_version = "2.6.1"
 
 describe "profile::letsencrypt" do
