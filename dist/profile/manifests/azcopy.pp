8c82de65ab448ab8c186bd84d772561eadfd34ef
Damien Duportal <damien.duportal@gmail.com>
2025-03-26 18:08:38 +0100

    cleanup(pkg) Cleanup update center related code (#3991)
    * chore(vagrant) add fixes to allow running pkg in Vagrant on a Silicon Mac
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * cleanup(pkg) remove 'updatesite' profile and 'updates' role
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * wip(azcopy) code review - cleanup
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- dist/profile/manifests/azcopy.pp
+++ dist/profile/manifests/azcopy.pp
@@ -5,11 +5,6 @@ class profile::azcopy (
   String $install_dir   = '/usr/local/bin',
 ) {
   include apt
-  # There is no linux_aarch64 azcopy release, considering that aarch64 = arm64 so vagrant can run on Mac Silicon
-  $architecture = $facts['os']['architecture'] ? {
-    'aarch64' => 'arm64',
-    default   => $facts['os']['architecture'],
-  }
 
   # Dependencies used to install azcopy
   ensure_packages([
@@ -18,22 +13,31 @@ class profile::azcopy (
   ])
 
   if $azcopy_version {
-    $msprodpackagepath = '/tmp/microsoft-prod.deb'
-    file { $msprodpackagepath:
-      ensure => 'file',
-      source => "https://packages.microsoft.com/config/${facts['os']['name'].downcase()}/${facts['os']['release']['full']}/packages-microsoft-prod.deb",
-    }
+    # Hack for Vagrant
+    if ( $facts['os']['architecture'] == 'aarch64' and $facts['os']['distro']['codename'] == 'bionic') {
+      exec { 'get-azcopy-bin':
+        require => [Package['curl'], Package['tar']],
+        command => '/usr/bin/curl --silent --show-error --location https://aka.ms/downloadazcopy-v10-linux-arm64 --output /tmp/azcopy.tgz && tar xzf /tmp/azcopy.tgz -C /tmp && find /tmp -type f -name azcopy -exec mv {} /usr/bin/ \; && rm -rf /tmp/azcopy*',
+        creates => '/usr/bin/azcopy',
+      }
+    } else {
+      $msprodpackagepath = '/tmp/microsoft-prod.deb'
+      file { $msprodpackagepath:
+        ensure => 'file',
+        source => "https://packages.microsoft.com/config/${facts['os']['name'].downcase()}/${facts['os']['release']['full']}/packages-microsoft-prod.deb",
+      }
 
-    package { 'microsoftprod':
-      ensure   => present,
-      source   => $msprodpackagepath,
-      provider => dpkg,
-      require  => File[$msprodpackagepath],
-    }
+      package { 'microsoftprod':
+        ensure   => present,
+        source   => $msprodpackagepath,
+        provider => dpkg,
+        require  => File[$msprodpackagepath],
+      }
 
-    package { 'azcopy':
-      ensure  => $azcopy_version,
-      require => [Class['apt::update'], Package['microsoftprod']],
+      package { 'azcopy':
+        ensure  => $azcopy_version,
+        require => [Class['apt::update'], Package['microsoftprod']],
+      }
     }
   }
 
