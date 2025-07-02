d4811c943b49a3fdff38d5cbbd6766e63aa7fa2a
Stéphane MERLE <95630726+smerle33@users.noreply.github.com>
2025-07-02 11:33:02 +0200

    feat(aws.ci.jenkins.io): setting back the ci on aws (#4213)
    * Revert "cleanup: remove aws.ci.jenkins.io and ci.jenkins.io-agents-2 (#4047)"
    
    This reverts commit b1b37e3768615b76c9cda025072658163419bdc1.
    
    * cherry pick jdk21 migration and comment cloud/ec2 agents
    
    * remove unecessary artifacts_manager
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * update manually amis
    
    * wip
    
    * wip

--- manifests/site.pp
+++ manifests/site.pp
@@ -66,6 +66,10 @@ node 'controller.ci.jenkins.io' {
   include role::jenkins::controller
 }
 
+node 'aws.ci.jenkins.io' {
+  include role::jenkins::controller
+}
+
 node 'controller.cert.ci.jenkins.io' {
   mount { '/var/lib/jenkins':
     ensure => 'mounted',
