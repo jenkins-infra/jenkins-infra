cbd58ec99c2d704b4d9534bf4b2570dc2022b9f4
Damien Duportal <damien.duportal@gmail.com>
2026-01-24 11:06:37 +0100

    hotfix(rspec/azcopy) fixup of b02d8220
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- spec/classes/profile/azcopy_spec.rb
+++ spec/classes/profile/azcopy_spec.rb
@@ -2,7 +2,5 @@ require "spec_helper"
 
 describe "profile::azcopy" do
   it { is_expected.to contain_package("azcopy") }
-  it { is_expected.to contain_package("curl") }
-  it { is_expected.to contain_package("tar") }
   it { is_expected.to contain_package("azure-cli") }
 end
