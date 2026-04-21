bae35c12d605f114613973a260c0a9517a78b85d
Damien Duportal <damien.duportal@gmail.com>
2026-04-21 15:46:29 +0200

    hotfix(letsencrypt) remove non-existent requirements
    Fixup of https://github.com/jenkins-infra/jenkins-infra/commit/af5ea1d293e3db58f1c252c35fc10d842308f972 where I was careless

--- dist/profile/manifests/letsencrypt.pp
+++ dist/profile/manifests/letsencrypt.pp
@@ -117,7 +117,6 @@ class profile::letsencrypt (
     $golang_installation_prefix='/usr/local'
 
     exec { "Install golang version ${golang_version}":
-      require => [Package['curl'],Package['tar']],
       command => "/usr/bin/curl --silent --location --verbose --show-error --output ${golang_dl_archive} \"${golang_dl_url}\" && /usr/bin/rm -rf ${golang_installation_prefix}/go && /usr/bin/tar -C ${golang_installation_prefix} -xzf ${golang_dl_archive} && /usr/bin/rm -f ${golang_dl_archive} && ln -s ${golang_installation_prefix}/go/bin/go /usr/local/bin/go",
       unless  => "/usr/bin/test -f /usr/local/bin/go && /usr/local/bin/go version 2>/dev/null | /bin/grep ${golang_version}",
     }
