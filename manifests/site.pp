26edca36a3c746a8027fe5cc8d5180d9765398d7
Damien Duportal <damien.duportal@gmail.com>
2026-09-05 15:22:40 +0200

    cleanup(cert.ci.jenkins.io) remove setup of the removed CDF controller VM
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,16 +64,6 @@ node 'aws.ci.jenkins.io' {
   include role::jenkins::controller
 }
 
-node 'controller.cert.ci.jenkins.io' {
-  mount { '/var/lib/jenkins':
-    ensure => 'mounted',
-    atboot => 'true',
-    device => 'UUID=afa01d2f-c643-4b0f-a917-66fedaee9325',
-    fstype => 'ext4',
-  }
-  include role::privateci
-}
-
 node 'controller-sponsored.cert.ci.jenkins.io' {
   mount { '/var/lib/jenkins':
     ensure => 'mounted',
